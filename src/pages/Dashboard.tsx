381c88badf9b2c9ab5cb35b5bfe3aa476d8de61c
Hypepad25 <joescyclesound@gmail.com>
2025-07-28 20:15:09 -0500

    ui update

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Dashboard = () => {
   return (
-    <div style={ padding: '2rem' }>
-      <h2 style={ color: 'orange' }>Dashboard Page</h2>
+    <div style={{ padding: '2rem' }}>
+      <h2 style={{ color: 'orange' }}>Dashboard Page</h2>
     </div>
   );
 };
