7eb7cb019fd4eb4cc11295c4838cc636c225846e
Hypepad25 <joescyclesound@gmail.com>
2025-07-29 18:32:44 -0500

    ui update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
-      <img src="/banner.png" alt="HYPEPAD Banner" className="w-full max-w-4xl" />
+      <img src="/hypepad-banner.jpg" alt="HYPEPAD Banner" className="w-full max-w-4xl" />
       <h1 className="text-4xl font-bold mt-6">The Future of Web3 Starts Here</h1>
     </div>
   )
